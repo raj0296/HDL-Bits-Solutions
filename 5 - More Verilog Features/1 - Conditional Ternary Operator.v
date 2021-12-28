3f417513e58c74cc0884bd0a9aa4bbc676f17946
raj0296 <96760359+raj0296@users.noreply.github.com>
2021-12-28 09:58:46 +0530

    make it in one line,no need to take extra wires

--- 5 - More Verilog Features/1 - Conditional Ternary Operator.v
+++ 5 - More Verilog Features/1 - Conditional Ternary Operator.v
@@ -2,10 +2,5 @@ module top_module (
     input [7:0] a, b, c, d,
     output [7:0] min);
     
-    wire [7:0] int1, int2;
-    
-    //condition ? if_true : if_false
-    assign int1 = (a<b)? a:b; 
-    assign int2 = (int1<c)? int1:c;
-    assign min  = (int2<d)? int2:d;
+    assign min=a>b?(c>b?(d>b?b:d):(c>d?d:c)):(c>a?(d>a?a:d):(c>d?d:c));
 endmodule
